e7d5ff2dec6fc82f31acd5dfe3c70656c92759a3
Siddiqov <smdplayer1999@gmail.com>
2021-02-10 19:59:12 +0500

    add navcontroller

--- app/src/main/java/uz/smd/itutor/ui/start/StartFragment.kt
+++ app/src/main/java/uz/smd/itutor/ui/start/StartFragment.kt
@@ -7,20 +7,24 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import dagger.hilt.android.AndroidEntryPoint
 import uz.smd.itutor.R
+
 /**
  * Created by Siddikov Mukhriddin on 2/10/21
  */
-class StartFragment:Fragment(R.layout.fragment_start) {
+@AndroidEntryPoint
+class StartFragment : Fragment(R.layout.fragment_start) {
     private val viewModel: StartVModel by viewModels()
+
     @SuppressLint("FragmentLiveDataObserve")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.ks.observe(this, Observer {
-            Toast.makeText(requireContext(),"salom",Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "salom", Toast.LENGTH_SHORT).show()
         })
         viewModel.ks.observe(this, Observer {
-            Toast.makeText(requireContext(),it,Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
         })
     }
 }
